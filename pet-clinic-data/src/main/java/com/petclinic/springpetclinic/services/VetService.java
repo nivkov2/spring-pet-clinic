76960dc0c5861f610dc644bf3ed5a0e65caf421f
Nikolay <ivkovnikolai@gmail.com>
2019-10-11 10:54:32 +0300

    Adding common base interface. Closes #11

--- pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.petclinic.springpetclinic.services;
 
 import com.petclinic.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
