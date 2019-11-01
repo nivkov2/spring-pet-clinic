50c90d263fb986ed52117cc977fcceae157519fa
Nikolay <ivkovnikolai@gmail.com>
2019-11-01 17:18:45 +0200

    Fixed broken links. Closes #44

--- pet-clinic-web/src/main/java/com/petclinic/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/petclinic/springpetclinic/controllers/VetController.java
@@ -6,22 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets"})
 @Controller
 public class VetController {
 
-  private final VetService vetService;
+    private final VetService vetService;
 
-  @Autowired
-  public VetController(VetService vetService) {
-    this.vetService = vetService;
-  }
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
-  @RequestMapping({"", "/", "/index", "/index.html"})
-  public String listVets(Model model) {
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model) {
 
-    model.addAttribute("vets", vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
 
-    return "vets/index";
-  }
+        return "vets/index";
+    }
 }
